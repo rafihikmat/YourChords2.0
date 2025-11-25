ae3d179104ad5e7d86fa4693007d28429e74d5f1
RJ <164978466+rafihikmat@users.noreply.github.com>
2025-11-26 06:02:04 +0700

    chore: Remove various JSON validation and fixing scripts and add `lib/data/guitar.json`.

--- remove_line.ps1
+++
@@ -1,8 +0,0 @@
-
-$file = "D:/WebsiteBaru/YourChords2.0/lib/data/guitar.json"
-$content = Get-Content $file
-# Line 60263 is index 60262. It contains "barres": [],
-# We want to remove it.
-$newContent = $content[0..60261] + $content[60263..($content.Length - 1)]
-$newContent | Set-Content $file -Encoding UTF8
-"Removed line 60263" | Out-File "D:/WebsiteBaru/YourChords2.0/fix_log.txt"
